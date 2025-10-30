2a8629676c51cb187f32bc3ce1b69511f77c5694
devaba <devaba@users.noreply.github.com>
2025-10-30 15:05:14 +0200

    Fix overlay path and add GNOME wallpaper override

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
-# Copy our wallpaper into the image
-cp -r files/* /
+#!/usr/bin/env bash
+set -oue pipefail
+
+# Copy our overlay into the image
+if [[ -d /tmp/build/files ]]; then
+  cp -r /tmp/build/files/* /
+fi
 
 # Set as default GNOME wallpaper
 mkdir -p /usr/share/glib-2.0/schemas
 cat > /usr/share/glib-2.0/schemas/90-aba-wallpaper.gschema.override <<'EOF'
 [org.gnome.desktop.background]
 picture-uri='file:///usr/share/backgrounds/aba-wallpaper.png'
+picture-uri-dark='file:///usr/share/backgrounds/aba-wallpaper.png'
 picture-options='zoom'
 EOF
+
 glib-compile-schemas /usr/share/glib-2.0/schemas
+
